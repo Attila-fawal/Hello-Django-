f167749272254f83bbbc0e8e83da4795c527f026
Attila fawal <attila_faweli@hotmail.com>
2023-06-14 16:32:27 +0000

    test

--- todo/views.py
+++ todo/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
-# Create your views here.
-def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
-
 
 def get_todo_list(request):
     items = Item.objects.all()
@@ -17,7 +13,7 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        form  = ItemForm(request.POST)
+        form = ItemForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
@@ -29,17 +25,17 @@ def add_item(request):
 
 
 def edit_item(request, item_id):
-     item = get_object_or_404(Item, id=item_id)
-     if request.method == 'POST':
-        form=ItemForm(request.POST, instance=item)
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
-     form = ItemForm(instance=item)
-     context = {
+    form = ItemForm(instance=item)
+    context = {
         'form': form
     }
-     return render(request, 'todo/edit_item.html', context)
+    return render(request, 'todo/edit_item.html', context)
 
 
 def toggle_item(request, item_id):
@@ -53,4 +49,3 @@ def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.delete()
     return redirect('get_todo_list')
-
